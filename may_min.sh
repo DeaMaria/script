e0ac95bef9345332d5ec5e206d678d8b087fe0df
DeaMaria <andreeamaria.birsan@yahoo.es>
2016-11-08 09:35:36 +0100

    script con error en la linea 3: "[: missing"

---
+++ may_min.sh
@@ -0,0 +1,23 @@
+echo "Introduce ruta a cambiar"
+read directorio
+	if ![ -d $directorio]; then
+		echo "No existe el directorio. Se utiliza el directorio actual"
+		
+		$directorio = "$( cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+	fi
+	cd $directorio
+
+for f in `ls`; do
+
+	g=`echo "$f" | tr '[:upper:]' '[:lower:]'`
+
+	echo $f,$g
+
+	if [ $f != $g ]; then
+
+		mv "$f" "$g"
+
+	fi
+
+done
